f615b25ec6d493cb4de5c51bdb26fa3820633875
kfwojton <kfwojton@gmail.com>
2016-07-22 14:36:16 -0400

    Slight changes to optimization

--- solution.py
+++ solution.py
@@ -1,5 +1,4 @@
 
-
 class Node(object):
     def __init__(self, data):
         self.data = data
@@ -8,27 +7,41 @@ class Node(object):
     def add_child(self, obj):
         self.children.append(obj)
 
-listo = [['null',3],[3,1],[1,5]]
-# give a list of two numbers each representing a parent child n-ary match
-
+listo = [['null',3],[3,1],[1,5],[3,4],[3,10]]
+#usage first_node = buld_nary_tree(listo)
 
 def build_n_ary_tree(listo):
     temp_list = []
-    counter = 1
 
     for ab in listo:
-        k = Node(ab[0])
-        k.add_child(ab[1])
-        if ab[0] == 'null':
-            starting_node = k
 
-    return starting_node
+        if ab[0] in temp_list:
 
-def navigate_tree(starting_node):
+
+            temp_list.append(ab[1])
+            navigate_tree(starting_nodea,ab[0],ab[1])
+
+        elif ab[0] == 'null':
+            f = Node(ab[0])
+            temp_list.append(ab[1])
+            starting_nodea = f
+            starting_nodea.add_child(Node(ab[1]))
+        else:
+            print '[has no parent node]' , ab[0]
+
+
+    return starting_nodea
+
+def navigate_tree(starting_node,look_for,add_value):
     if starting_node.children:
         for ab in starting_node.children:
             starting_node =  ab
-            navigate_tree(starting_node)
-        else:
-            #no more children!
-            pass
+            if look_for == ab.data:
+                ab.add_child(Node(add_value))
+                pass
+            else:
+                navigate_tree(starting_node,look_for,add_value)
+
+    else:
+
+        pass
